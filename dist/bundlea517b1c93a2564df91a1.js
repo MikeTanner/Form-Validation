594929828da2f4e9f06e9335e404e19aa8fe93a2
Mike Tanner <miketannercs@gmail.com>
2022-12-22 19:56:59 -0600

    error formated, email complete, need coloring

--- dist/bundlea517b1c93a2564df91a1.js
+++ dist/bundlea517b1c93a2564df91a1.js
@@ -1,77 +1,87 @@
-/******/ (() => { // webpackBootstrap
-/******/ 	var __webpack_modules__ = ({
-
-/***/ "./src/assets sync \\.(gif%7Cpng%7Cjpe?g%7Csvg)$":
-/*!********************************************************************!*\
+/******/ (() => {
+  // webpackBootstrap
+  /******/ var __webpack_modules__ = {
+    /***/ "./src/assets sync \\.(gif%7Cpng%7Cjpe?g%7Csvg)$":
+      /*!********************************************************************!*\
   !*** ./src/assets/ sync nonrecursive \.(gif%7Cpng%7Cjpe?g%7Csvg)$ ***!
   \********************************************************************/
-/***/ ((module) => {
+      /***/ (module) => {
+        function webpackEmptyContext(req) {
+          var e = new Error("Cannot find module '" + req + "'");
+          e.code = "MODULE_NOT_FOUND";
+          throw e;
+        }
+        webpackEmptyContext.keys = () => [];
+        webpackEmptyContext.resolve = webpackEmptyContext;
+        webpackEmptyContext.id =
+          "./src/assets sync \\.(gif%7Cpng%7Cjpe?g%7Csvg)$";
+        module.exports = webpackEmptyContext;
 
-function webpackEmptyContext(req) {
-	var e = new Error("Cannot find module '" + req + "'");
-	e.code = 'MODULE_NOT_FOUND';
-	throw e;
-}
-webpackEmptyContext.keys = () => ([]);
-webpackEmptyContext.resolve = webpackEmptyContext;
-webpackEmptyContext.id = "./src/assets sync \\.(gif%7Cpng%7Cjpe?g%7Csvg)$";
-module.exports = webpackEmptyContext;
+        /***/
+      },
 
-/***/ })
-
-/******/ 	});
-/************************************************************************/
-/******/ 	// The module cache
-/******/ 	var __webpack_module_cache__ = {};
-/******/ 	
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/ 		// Check if module is in cache
-/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
-/******/ 		if (cachedModule !== undefined) {
-/******/ 			return cachedModule.exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = __webpack_module_cache__[moduleId] = {
-/******/ 			// no module.id needed
-/******/ 			// no module.loaded needed
-/******/ 			exports: {}
-/******/ 		};
-/******/ 	
-/******/ 		// Execute the module function
-/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
-/******/ 	
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/ 	
-/************************************************************************/
-/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
-/******/ 	(() => {
-/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
-/******/ 	})();
-/******/ 	
-/************************************************************************/
-var __webpack_exports__ = {};
-// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
-(() => {
-/*!**********************!*\
+    /******/
+  };
+  /************************************************************************/
+  /******/ // The module cache
+  /******/ var __webpack_module_cache__ = {};
+  /******/
+  /******/ // The require function
+  /******/ function __webpack_require__(moduleId) {
+    /******/ // Check if module is in cache
+    /******/ var cachedModule = __webpack_module_cache__[moduleId];
+    /******/ if (cachedModule !== undefined) {
+      /******/ return cachedModule.exports;
+      /******/
+    }
+    /******/ // Create a new module (and put it into the cache)
+    /******/ var module = (__webpack_module_cache__[moduleId] = {
+      /******/ // no module.id needed
+      /******/ // no module.loaded needed
+      /******/ exports: {},
+      /******/
+    });
+    /******/
+    /******/ // Execute the module function
+    /******/ __webpack_modules__[moduleId](
+      module,
+      module.exports,
+      __webpack_require__
+    );
+    /******/
+    /******/ // Return the exports of the module
+    /******/ return module.exports;
+    /******/
+  }
+  /******/
+  /************************************************************************/
+  /******/ /* webpack/runtime/hasOwnProperty shorthand */
+  /******/ (() => {
+    /******/ __webpack_require__.o = (obj, prop) =>
+      Object.prototype.hasOwnProperty.call(obj, prop);
+    /******/
+  })();
+  /******/
+  /************************************************************************/
+  var __webpack_exports__ = {};
+  // This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+  (() => {
+    /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-const cache = {};
-function importAll(r) {
-  // eslint-disable-next-line no-return-assign
-  r.keys().forEach((key) => (cache[key] = r(key)));
-}
-const images = importAll(
-  __webpack_require__("./src/assets sync \\.(gif%7Cpng%7Cjpe?g%7Csvg)$")
-);
-console.log("test");
-console.log("greg");
-console.log(images);
+    const cache = {};
+    function importAll(r) {
+      // eslint-disable-next-line no-return-assign
+      r.keys().forEach((key) => (cache[key] = r(key)));
+    }
+    const images = importAll(
+      __webpack_require__("./src/assets sync \\.(gif%7Cpng%7Cjpe?g%7Csvg)$")
+    );
+    console.log("test");
+    console.log("greg");
+    console.log(images);
+  })();
 
+  /******/
 })();
-
-/******/ })()
-;
 //# sourceMappingURL=bundlea517b1c93a2564df91a1.js.map
--- src/index.js
+++ src/index.js
@@ -1,13 +1,46 @@
 import "./styles/main.css";
 
-const cache = {};
-function importAll(r) {
-  // eslint-disable-next-line no-return-assign
-  r.keys().forEach((key) => (cache[key] = r(key)));
-}
-const images = importAll(
-  require.context("./assets", false, /\.(gif|png|jpe?g|svg)$/)
-);
-console.log("test");
-console.log("greg");
-console.log(images);
+const country = document.getElementById("country");
+const zipCode = document.getElementById("zipCode");
+const password = document.getElementById("password");
+const passwordConfirm = document.getElementById("passwordConfirm");
+const form = document.querySelector("form");
+
+
+
+
+
+const emailVal = (() => {
+  const email = document.getElementById("email");
+  const errorMsg = document.querySelector(".emailError");
+  const showError = () => {
+    if (email.validity.valueMissing) {
+      errorMsg.textContent = "You need to enter email";
+    }
+  };
+  const addListener = () => {
+    email.addEventListener("input", () => {
+      console.log(email.validity);
+      
+      if (email.validity.valid) {
+        console.log("valid");
+        errorMsg.textContent = "";
+      } else {        
+        showError();
+      }
+    });
+
+  }
+  return {
+    addListener
+  }
+})()
+const formHandler = (() => {
+  const startup = () => {
+    emailVal.addListener()
+  }
+  return {
+    startup
+  }
+})()
+formHandler.startup()
